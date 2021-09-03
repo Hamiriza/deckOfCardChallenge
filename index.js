ca7ec2bc2d94989894efcd41083dad3b1477a78f
Hamiriza Firdhan Ardyasta <hamirizafirdhanardyasta@itsss-MacBook-Pro.local>
2021-09-03 12:40:44 +0800

    changed data structure to linked list

--- index.js
+++ index.js
@@ -1,4 +1,92 @@
 const readline = require("readline");
+
+/*------------- Doubly linked list data structure -----------------*/
+function Node(value, next, prev) {
+  this.value = value;
+  this.next = next;
+  this.prev = prev;
+}
+
+function LinkedList(head, tail) {
+  this.head = null;
+  this.tail = null;
+
+  this.addToHead = function (value) {
+    let node = new Node(value, null, this.head);
+    if (this.head) this.head.next = node;
+    else this.tail = node;
+    this.head = node;
+  };
+
+  this.addToTail = function (value) {
+    let node = new Node(value, this.tail, null);
+    if (this.tail) this.tail.prev = node;
+    else this.head = node;
+    this.tail = node;
+  };
+
+  this.removeHead = function () {
+    if (!this.head) return null;
+    let value = this.head.value;
+    this.head = this.head.prev;
+    if (this.head) this.head.next = null;
+    return value;
+  };
+
+  this.removeTail = function () {
+    if (!this.tail) return null;
+    let value = this.tail.value;
+    this.tail = this.tail.next;
+    if (this.tail) this.tail.prev = null;
+    return value;
+  };
+
+  this.search = function (value) {
+    let current = this.head;
+    while (current) {
+      if (current.value == value) return value;
+      current = current.prev;
+    }
+    return null;
+  };
+
+  this.indexOf = function (value) {
+    let indexes = [];
+    let current = this.tail;
+    let index = 0;
+    while (current) {
+      if (current.value == value) indexes.push(index);
+      current = current.next;
+      index++;
+    }
+    return indexes;
+  };
+
+  this.findSize = function () {
+    let node = this.tail;
+    let res = 0;
+    while (node !== null) {
+      res++;
+      node = node.next;
+    }
+    return res;
+  };
+
+  this.getElement = function (index) {
+    let current = this.tail;
+    let count = 0;
+
+    while (current !== null) {
+      if (count == index) return current.value;
+      count++;
+      current = current.next;
+    }
+    return null;
+  };
+}
+
+/*-----------------------------------------------------------------*/
+
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
@@ -19,16 +107,21 @@ rl.question("How many cards are there in the deck? ", function (size) {
 });
 
 function calculate(size) {
-  let originalArray = Array.from({ length: size }, (undefined, i) => i);
-  let changedArray = firstRound(originalArray);
+  let originalDeck = new LinkedList();
+
+  for (let i = 0; i < size; i++) {
+    originalDeck.addToHead(i);
+  }
+
+  let shuffledDeck = firstRound(originalDeck);
   let result = 1;
 
-  for (let i = 0; i < changedArray.length; i++) {
+  for (let i = 0; i < shuffledDeck.findSize(); i++) {
     let rotationRounds = 1;
     let rotate = i;
 
-    while (changedArray[rotate] !== i) {
-      rotate = changedArray[rotate];
+    while (shuffledDeck.getElement(rotate) !== i) {
+      rotate = shuffledDeck.getElement(rotate);
       rotationRounds++;
     }
 
@@ -38,14 +131,24 @@ function calculate(size) {
   return result;
 }
 
-function firstRound(array) {
-  let table = [];
-  let hand = [...array];
-  while (hand.length > 1) {
-    table.unshift(hand.shift());
-    hand.push(hand.shift());
+function firstRound(deck) {
+  let table = new LinkedList();
+  let hand = new LinkedList();
+  let current = deck.tail;
+
+  //copy the original deck to hand to prevent mutation
+  while (current) {
+    hand.addToHead(current.value);
+    current = current.next;
   }
-  table.unshift(hand.shift());
+
+  //do a first round of shuffle
+  while (hand.findSize() !== 0) {
+    table.addToTail(hand.removeTail());
+    hand.addToHead(hand.removeTail());
+    if (hand.findSize() == 0) table.addToTail(hand.removeHead());
+  }
+
   return table;
 }
 
